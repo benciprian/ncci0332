4b19475f3582f7546395cd6dd1c3665812b3ba1e
benciprian <ben27ciprian@gmail.com>
2023-11-01 15:53:22 +0200

    second commit

--- src/test/java/AngajatiApp/repository/TC1_add_Employee.java
+++ src/test/java/AngajatiApp/repository/TC1_add_Employee.java
@@ -2,21 +2,26 @@ package AngajatiApp.repository;
 
 import AngajatiApp.controller.DidacticFunction;
 import AngajatiApp.model.Employee;
+import AngajatiApp.validator.EmployeeValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TC1_add_Employee {
     private EmployeeMock employeeMock;
+    private List<Employee> employeeList;
+    private EmployeeValidator employeeValidator;
 
     @BeforeEach
     void setUp() {
         employeeMock = new EmployeeMock();
     }
 
-    @Test
+   /* @Test
     void TC1_addEmployee() {
 
         Employee newEmployee = new Employee();
@@ -37,6 +42,12 @@ class TC1_add_Employee {
         // Check if the new employee is in the list
         assertTrue(employeeMock.getEmployeeList().contains(newEmployee));
 
-    }
+    }*/
+   @Test
+   void TC1_addEmployee() {
+       Employee en = new Employee(5, "Flavius", "Popa", "1681234567590", DidacticFunction.ASISTENT, 6800.00);
+       boolean result = employeeMock.addEmployee(en);
+       assertEquals(true, result);
+   }
 }
 
